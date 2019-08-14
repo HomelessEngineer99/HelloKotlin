c0566851345fe6bd8bfda8265777f3ed12186d1f
homelessengineer99 <homelessengineer99@gmail.com>
2019-08-14 11:44:12 -0700

    added functions to test Log class

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -2,12 +2,41 @@ package com.example.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.*
+
+const val TAG = "MainActivity"
+const val MSGC = "onCreate"
+const val MSGR = "onResume"
+const val MSGS = "onStart"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val strings = arrayOf("one", "four")
+        println(strings[1])
+        Log.d(TAG, MSGC)
+
+        doSomething()
+
+    }
+
+    fun doSomething() {
+        println("Hello!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, MSGS)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, MSGR)
+        println("Hello")
 
     }
 }
